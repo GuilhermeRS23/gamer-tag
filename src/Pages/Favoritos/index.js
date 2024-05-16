41f41217627be1d491fe3ffa1d971768e55746f4
GuilhermeRS <guilhermemuralha2016@gmail.com>
2024-05-16 10:04:38 -0300

    Resolvendo desafio - Aula 2

--- src/Pages/Favoritos/index.js
+++ src/Pages/Favoritos/index.js
@@ -1,8 +1,27 @@
+import Banner from 'Components/Banner';
 import style from './Favoritos.module.css';
+import Titulo from 'Components/Titulo';
+import Card from 'Components/Card';
 
-const Favoritos = ()=>{
+import jogos from 'json/db.json'
+
+const Favoritos = ( )=>{
+    //Código provisório
+    const favorito = jogos.sort((a,b)=> b.id - a.id)
+    .slice(0, 3);
+
     return(
         <>
+        <Banner imagem={"favoritos"}/>
+        <Titulo>
+            <h1>Meus Jogos Favoritos.</h1>
+        </Titulo>
+        {/* Código provisório */}
+        <section className={style.container}>
+                {favorito.map((jogo) => {
+                    return <Card {...jogo} key={jogo.id} />
+                })}
+            </section>
         </>
     )
 }
