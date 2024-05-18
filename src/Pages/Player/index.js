9a1dfd4c3e334a3455f0d411c2aa1119cfe23949
Guilherme R.Silva <guilhermers2014@outlook.com>
2024-05-18 09:11:15 -0300

    05 - Externalizando dados

--- src/Pages/Player/index.js
+++ src/Pages/Player/index.js
@@ -3,15 +3,22 @@ import Banner from 'Components/Banner';
 import Titulo from 'Components/Titulo';
 import Erro404 from 'Pages/Erro404';
 import jogosVideos from 'json/db.json';
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 const Player = () => {
+    const [video, setVideo] = useState();
     const parametros = useParams();
-    const video = jogosVideos.find((video) => {
-        return video.id === Number(parametros.id);
-    })
 
-    if(!video){
+    useEffect(() => {
+        fetch(`https://my-json-server.typicode.com/GuilhermeRS23/gamer-tag-api/jogos?id=${parametros.id}`)
+            .then(resposta => resposta.json())
+            .then(dados => {
+                setVideo(...dados)
+            })
+    }, []);
+
+    if (!video) {
         return < Erro404 />
     }
 
@@ -22,11 +29,11 @@ const Player = () => {
                 <h1>Player</h1>
             </Titulo >
             <section className={style.container}>
-            <iframe frameBorder="0" 
-            width="100%" height="100%" 
-            src={video.link} 
-            title={video.titulo} 
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                <iframe frameBorder="0"
+                    width="100%" height="100%"
+                    src={video.link}
+                    title={video.titulo}
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
             </section>
         </>
     )
